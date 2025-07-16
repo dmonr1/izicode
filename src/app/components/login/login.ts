710c040d716572201b06a7b29b55737ca24ac490
diegor <diegorutti26@gmail.com>
2025-07-16 15:03:14 -0500

    incio preguntasr

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -26,32 +26,107 @@ export class Login implements OnInit {
 
   avatars: string[] = [
     'assets/nico-1.png',
-    'assets/avatar_centrado.png',
-    'assets/nico2.png'
+    'assets/avatar_centrado_recortado.png',
+    'assets/nico2.png',
   ];
+
+  currentIndex = 1;
+
+  typingSound = new Audio('assets/videoplayback.mp3');
+  startupSound = new Audio('assets/entrada.mp3');
+
+  isTyping = false;
+
+  get visibleAvatars(): string[] {
+    const len = this.avatars.length;
+    const left = (this.currentIndex - 1 + len) % len;
+    const center = this.currentIndex;
+    const right = (this.currentIndex + 1) % len;
+
+    return [this.avatars[left], this.avatars[center], this.avatars[right]];
+  }
 
   ngOnInit(): void {
     this.typeLine(this.line1, 1, () => {
       this.showCursor1 = false;
       this.showCursor2 = true;
-  
+
       this.typeLine(this.line2, 2, () => {
         this.showCursor2 = false;
-  
+
         setTimeout(() => this.showAuthor = true, 300);
         setTimeout(() => this.showButton = true, 1200);
       });
     });
-  
-   }
+
+  }
+
+
+  fondoActual: string = '#1d1c1c'; // color inicial
+
+  initScrollAndKeyListeners(): void {
+    window.addEventListener('keydown', (e: KeyboardEvent) => {
+      if (e.key === 'ArrowRight' && this.canGoRight()) {
+        this.rotateCarouselDown();
+      } else if (e.key === 'ArrowLeft' && this.canGoLeft()) {
+        this.rotateCarouselUp();
+      }
+    });
+
+    let lastScrollTime = 0;
+    window.addEventListener('wheel', (e: WheelEvent) => {
+      const now = Date.now();
+      if (now - lastScrollTime < 300) return;
+
+      if (e.deltaX > 10 || e.deltaY < -10) {
+        if (this.canGoRight()) {
+          this.rotateCarouselDown();
+          lastScrollTime = now;
+        }
+      } else if (e.deltaX < -10 || e.deltaY > 10) {
+        if (this.canGoLeft()) {
+          this.rotateCarouselUp();
+          lastScrollTime = now;
+        }
+      }
+    }, { passive: true });
+  }
+
+  canGoLeft(): boolean {
+    return this.currentIndex > 0;
+  }
+
+  canGoRight(): boolean {
+    return this.currentIndex < this.avatars.length - 1;
+  }
+
+  rotateCarouselDown() {
+    if (this.canGoRight()) {
+      this.currentIndex++;
+    }
+  }
+
+  rotateCarouselUp() {
+    if (this.canGoLeft()) {
+      this.currentIndex--;
+    }
+  }
 
 
   expandirCirculo() {
+    this.startupSound.play().catch(() => { });
     this.avatarVisible = true;
+
+    setTimeout(() => this.initScrollAndKeyListeners(), 1500);
   }
 
   typeLine(text: string, line: number, callback: () => void) {
     let i = 0;
+
+    this.typingSound.loop = true;
+    this.typingSound.volume = 0.8;
+    this.typingSound.play().catch(() => { });
+
     const interval = setInterval(() => {
       if (i < text.length) {
         if (line === 1) this.typedLine1 += text[i];
@@ -59,8 +134,41 @@ export class Login implements OnInit {
         i++;
       } else {
         clearInterval(interval);
+        this.typingSound.pause();
+        this.typingSound.currentTime = 0;
         callback();
       }
     }, 100);
   }
+
+  mostrarPregunta: boolean = false;
+  pantallaInundada: boolean = false;
+
+  irAPregunta() {
+    this.pantallaInundada = true;
+    setTimeout(() => {
+      this.mostrarPregunta = true;
+    }, 1200); 
+  }
+
+  avatarSeleccionado(i: number) {
+    this.avatarVisible = false;
+    this.showButton = false;
+    this.showAuthor = false;
+    this.showCursor1 = false;
+    this.showCursor2 = false;
+  
+    this.pantallaInundada = true;
+  
+    // Espera a que suba y baje la ola antes de mostrar la pregunta
+    setTimeout(() => {
+      this.fondoActual = 'rgb(255 177 177)'; // o el color que usaste en la ola que baja
+      this.mostrarPregunta = true;
+      this.pantallaInundada = false; // oculta la animación
+    }, 2000); // espera que termine el efecto visual
+  }
+  
+
+
 }
+
